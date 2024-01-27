e6411b0fc4d93047ef3e7778ff3269d37d906805
SpideyFanDan <dansmith.coder@gmail.com>
2024-01-27 15:58:59 -0700

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module wgu.dansmithc195project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens wgu.dansmithc195project to javafx.fxml;
+    exports wgu.dansmithc195project;
+}
