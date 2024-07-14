8c5fe723e6a68a921ce6e54c84c8aff09c7fa358
Dan Smith <dwittysmitty@gmail.com>
2024-07-14 17:06:34 -0600

    added method for authenticating user

--- src/main/java/wgu/dansmithc195project/DOA/UserDAOImpl.java
+++ src/main/java/wgu/dansmithc195project/DOA/UserDAOImpl.java
@@ -9,6 +9,9 @@ import java.sql.*;
 
 /**This clas is connecting to the users database with SQL queries*/
 public class UserDAOImpl {
+    /**This method gets all users from SQL database
+     * @return usersList
+     */
     public static ObservableList<User> getAllUsers(){
         ObservableList<User> usersList = FXCollections.observableArrayList();
         try {
@@ -27,4 +30,26 @@ public class UserDAOImpl {
         }
         return usersList;
     }
+    /***/
+    public static ObservableList<User> authenticateUser(String username, String inputPassword){
+        ObservableList<User> usersList = FXCollections.observableArrayList();
+        try {
+            String sql = "Select * from users WHERE User_Name = ? AND Password = ?";
+            PreparedStatement preparedStatement = JDBC.connection.prepareStatement(sql);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, inputPassword);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                int userId = resultSet.getInt("User_ID");
+                String userName = resultSet.getString("User_Name");
+                String password = resultSet.getString("Password");
+                User user = new User(userId, userName, password);
+                usersList.add(user);
+            }
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return usersList;
+    }
+
 }
